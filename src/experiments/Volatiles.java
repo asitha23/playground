8dd0a3542fe64010a26dfe556a64d22359dacd71
asitha <asithawicumsiri@gmail.com>
2026-01-24 19:01:15 +0800

    volatile examples

---
+++ src/experiments/Volatiles.java
@@ -0,0 +1,37 @@
+package experiments;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Volatiles {
+    static boolean flag = true;
+
+    void main () {
+        Runnable runnable = () -> {
+            while (flag) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "loop running");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+            }
+            System.out.println(Thread.currentThread().getName() + "loop finished");
+        };
+        Runnable runnableFlagUpdate = () -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            flag = false;
+            System.out.println(Thread.currentThread().getName() + "flag updated");
+        };
+
+        try (ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())){
+            executor.submit(runnable);
+            executor.submit(runnableFlagUpdate);
+        }
+
+
+    }
+
+}
