320687330ecc97f690425e3f22b54c408bbf776e
asitha <asithawicumsiri@gmail.com>
2026-02-09 10:44:28 +0800

    add InsertionSort

---
+++ src/main/java/experiments/InsertionSort.java
@@ -0,0 +1,34 @@
+package experiments;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static java.lang.IO.print;
+import static java.lang.IO.println;
+
+public class InsertionSort {
+
+    void main() {
+      int[] array = new int[10];
+      Random rand = new Random();
+      for (int i = 0; i < array.length; i++) {
+          array[i] = rand.nextInt(100);
+      }
+
+      println("before sort" + Arrays.toString(array));
+      insertSort(array);
+      println("after sort" + Arrays.toString(array));
+    }
+
+    private void insertSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
