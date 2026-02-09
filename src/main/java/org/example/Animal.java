f1f819a77e18a27a697dba38a0c26ac2c29ef772
asitha <asithawicumsiri@gmail.com>
2026-02-09 09:31:17 +0800

    add classes

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static java.lang.IO.println;
+
+public class Animal {
+
+    protected void makeNoise() {
+        println("make animal sound");
+    }
+
+}
---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static java.lang.IO.println;
+
+public class Dog extends Animal {
+
+
+    @Override
+    public void makeNoise() {
+       println("bow boow");
+    }
+}
