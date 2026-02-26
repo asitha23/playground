1788df7586a77c66bde1964882a68820b1e2d203
asitha <asithawicumsiri@gmail.com>
2026-02-26 22:49:27 +0800

    updated StreamMethods

--- src/main/java/streamapi/StreamMethods.java
+++ src/main/java/streamapi/StreamMethods.java
@@ -1,7 +1,9 @@
 package streamapi;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.lang.IO.println;
@@ -10,12 +12,15 @@ public class StreamMethods {
 
 
     List<Integer> list = List.of(1, 2, 3, 4, 5);
+    List<List<Integer>> listOfList = List.of(List.of(1, 2, 3), List.of(4, 5, 6),  List.of(7, 8, 9));
+
 
     void main() {
         streamReduce();
         streamMaxMin();
         streamIterate();
         streamGatherSlidingWindow();
+        mapToMap();
 
         streamReuseException();
     }
@@ -55,5 +60,22 @@ public class StreamMethods {
         list.stream()
                 .gather(PrimeBatcher.gatherPrimes(4))
                 .forEach(System.out::println);
+    }
+
+    void streamFaltList() {
+        println("streamFaltMap");
+        listOfList.forEach(System.out::println);
+        List<Integer> s = listOfList.stream().flatMap(List::stream).toList();
+        s.forEach(System.out::println);
+    }
+
+    void mapToMap() {
+        println("mapToMap");
+        Map<String, Integer> originalMap = Map.of("a", 1, "b", 2, "c", 3, "d", 4);
+        originalMap.forEach((k,v) -> println(k + " : " + v));
+        Map<String, String> transformdMap = originalMap.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> Integer.toBinaryString(entry.getValue())));
+        transformdMap.forEach((k, v) -> println(k + " : " + v));
+
     }
 }
