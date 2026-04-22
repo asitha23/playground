edf656fa28ebbd52b5228bfab7e010e5f86f8cda
asitha <asithawicumsiri@gmail.com>
2026-04-22 13:50:04 +0800

    EverySubArray class updated

--- src/main/java/experiments/algo/EverySubArray.java
+++ src/main/java/experiments/algo/EverySubArray.java
@@ -6,32 +6,23 @@ import static java.lang.IO.println;
 public class EverySubArray {
 
     public void printEverySubArray(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = nums.length -1 - i; j <= 0; j--) {
+        int n = nums.length;
 
-                for (int k = 0; k <= j; k++) {
-                    print(nums[k]);
+        // Outer loop to pick the starting index 'i'
+        for (int i = 0; i < n; i++) {
+            // Middle loop to pick the ending index 'j' (starting from 'i')
+            for (int j = i; j < n; j++) {
+                // Innermost loop to print/process elements from 'i' to 'j'
+                System.out.print("[");
+                for (int k = i; k <= j; k++) {
+                    System.out.print(nums[k]);
+                    if (k < j) {
+                        System.out.print(", ");
+                    }
                 }
-                println("");
+                System.out.println("]");
             }
         }
-//        int n = nums.length;
-//
-//        // Outer loop to pick the starting index 'i'
-//        for (int i = 0; i < n; i++) {
-//            // Middle loop to pick the ending index 'j' (starting from 'i')
-//            for (int j = i; j < n; j++) {
-//                // Innermost loop to print/process elements from 'i' to 'j'
-//                System.out.print("[");
-//                for (int k = i; k <= j; k++) {
-//                    System.out.print(nums[k]);
-//                    if (k < j) {
-//                        System.out.print(", ");
-//                    }
-//                }
-//                System.out.println("]");
-//            }
-//        }
 
     }
 
